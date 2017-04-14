bcb6103b0d596f114ead800cf37d90ee1c40a471
DenisDerkach <derkachdenisvladimirovich@gmail.com>
2017-04-14 17:35:46 +0300

    Create guess.c

---
+++ guess.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int guess = 50;
+	char response;
+	printf("Choose an integer in the range from 1 to 100. I'll try to guess ");
+	printf("it. \nPress the y key, if my guess is correct and ");
+	printf("\nthe n key otherwise.\n");
+	printf("Is your number %d?\n", guess);
+	
+	while ((response = getchar()) != 'y')		/* получить ответ */
+	{
+		if (response == 'n')
+			printf("Okay, then it's %d?\n", ++guess);
+		else
+			printf("Only y or n options are accepted.\n");
+		while (getchar() != '\n')				/* пропустить оставшуюся часть входной строки */
+			continue;
+	}
+	printf("I knew that I would succeed!\n");
+	return 0;
+}
